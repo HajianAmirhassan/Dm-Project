9e09869d508238579a4b01c9b03630ea390b453f
Amirhassan HajianManesh <124922307+HajianAmirhassan@users.noreply.github.com>
2025-01-07 17:09:56 +0330

    merge code

--- Project.py
+++ Project.py
@@ -1,3 +1,34 @@
+def SplitBinary(s, max_len=3):
+    def backtrack(start, Current):
+        if start == len(s):
+            result.append(Current[:])
+            return
+        
+        for length in range(1, min(max_len + 1, len(s) - start + 1)):
+            substring = s[start:start + length]
+            Current.append(substring)
+            backtrack(start + length, Current)
+            Current.pop()
+    
+    result = []
+    backtrack(0, [])
+    return result
+
+def hasPrefixInArray(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if i != j and arr[j].startswith(arr[i]) and arr[i] != arr[j]:
+                return True
+    return False
+
+def Filter(splits):
+    ArrayOut = []
+    for split in splits:
+        hasPrefix = hasPrefixInArray(split)
+        if not hasPrefix:
+            ArrayOut.append(split)
+    return ArrayOut
+
 def Validation():
     user_input = input("")
     for char in user_input:
@@ -15,21 +46,18 @@ def CalculateX(n):
       result *= (26 - i)
     return result
 
-def part2(input):
-    print("part2")
-
-
 input_string = Validation()
-# print("You entered:", input_string)
 
-# listLen = part2(input_string)
+allSplits = SplitBinary(input_string)
+valid = Filter(allSplits)
+for i in range (len(valid)):
+    valid[i] = set(valid[i])
 
-
-listLen = [ 1 , 2 ,2]
+lengths = [len(item) for item in valid]
 
 Sum = 0
-for i in listLen:
+for i in lengths:
     Sum += CalculateX(i)
 
-print(len(listLen))
+print(len(lengths))
 print(Sum)
